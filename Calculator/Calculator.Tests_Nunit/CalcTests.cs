310bf4f150c49726f4d1d87d307d4d8a346c0db3
André <AndreR@ppedv.de>
2023-02-06 15:41:40 +0100

    Tests mit Nunit

---
+++ Calculator/Calculator.Tests_Nunit/CalcTests.cs
@@ -0,0 +1,32 @@
+namespace Calculator.Tests_Nunit
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Sum_3_and_4_results_7()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(3, 4);
+
+            //Assert
+            //Assert.AreEqual(7, result);
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        [TestCase(int.MaxValue, 1)]
+        [TestCase(1, int.MaxValue)]
+        [TestCase(int.MinValue, -1)]
+        [TestCase(-1, int.MinValue)]
+        public void Sum_throws_OverflowExceptions(int a, int b)
+        {
+            var calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(a, b));
+        }
+    }
+}
