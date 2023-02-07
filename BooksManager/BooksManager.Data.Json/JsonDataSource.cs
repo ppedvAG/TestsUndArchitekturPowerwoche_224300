df5bfa24ff0c9de5ba7ecffc96bd602c14a0eb62
André <AndreR@ppedv.de>
2023-02-07 15:44:34 +0100

    DataSource JSON

---
+++ BooksManager/BooksManager.Data.Json/JsonDataSource.cs
@@ -0,0 +1,25 @@
+﻿using BooksManager.Common;
+using System.Text.Json;
+
+namespace BooksManager.Data.Json
+{
+    public class JsonDataSource : IDataSource
+    {
+        public string FilePath { get; }
+
+        public JsonDataSource(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return JsonSerializer.Deserialize<IEnumerable<Book>>(FilePath);
+        }
+
+        public void KillAllHumans()
+        {
+
+        }
+    }
+}
