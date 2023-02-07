ac3621f5aec91a021c4414143a39496bcc30adf9
André <AndreR@ppedv.de>
2023-02-07 10:44:56 +0100

    OpeningHourse Tests

---
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TddBank.Tests
+{
+    public class OpeningHoursTests
+    {
+        [Theory]
+        [InlineData(2023, 2, 06, 10, 30, true)]//mo
+        [InlineData(2023, 2, 06, 10, 29, false)]//mo
+        [InlineData(2023, 2, 06, 10, 31, true)] //mo
+        [InlineData(2023, 2, 06, 18, 59, true)] //mo
+        [InlineData(2023, 2, 06, 19, 00, false)] //mo
+        [InlineData(2023, 2, 11, 13, 0, true)] //sa
+        [InlineData(2023, 2, 11, 16, 0, false)] //sa
+        [InlineData(2023, 2, 12, 20, 0, false)] //so
+        public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+
+            Assert.Equal(result, oh.IsOpen(dt));
+        }
+    }
+}
