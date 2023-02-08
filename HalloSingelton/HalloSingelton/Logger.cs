81e7d044c0da82c8e0c407f452477d6573fb2d0d
André <AndreR@ppedv.de>
2023-02-08 10:19:48 +0100

    Singelton Pattern

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,31 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static object _syncObj = new object();
+        private static Logger _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_syncObj)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Log("Logger wurde gestartet");
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now:G}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 20; i++)
+{
+    Task.Run(() => Logger.Instance.Log($"Hallo Logger {i:00}"));
+}
+
+Logger.Instance.Log("Hallo Logger");
