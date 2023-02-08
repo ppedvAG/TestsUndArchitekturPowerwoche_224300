0ab8d45498d19ab037a20c94f3addc70972eca13
André <AndreR@ppedv.de>
2023-02-08 12:37:26 +0100

    Dekopattern

---
+++ HalloDekorator/HalloDekorator/Deko.cs
@@ -0,0 +1,56 @@
+﻿namespace HalloDekorator
+{
+    internal interface IComponent
+    {
+        decimal Preis { get; }
+        string Beschreibung { get; }
+    }
+
+    class Pizza : IComponent //ConcreteComponent
+    {
+        public decimal Preis => 6.5m;
+
+        public string Beschreibung => "Pizza";
+    }
+
+    class Brot : IComponent //ConcreteComponent
+    {
+        public decimal Preis => 3.2m;
+
+        public string Beschreibung => "Brot";
+    }
+
+    abstract class Deko : IComponent
+    {
+        protected IComponent _parent;
+
+        protected Deko(IComponent parent)
+        {
+            _parent = parent;
+        }
+
+        public abstract decimal Preis { get; }
+
+        public abstract string Beschreibung { get; }
+    }
+
+    class Käse : Deko
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override decimal Preis => _parent.Preis + 1.5m;
+
+        public override string Beschreibung => _parent.Beschreibung + " Käse";
+    }
+
+    class Salami : Deko
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override decimal Preis => _parent.Preis + 2.7m;
+
+        public override string Beschreibung => _parent.Beschreibung + " Salami";
+    }
+}
---
+++ HalloDekorator/HalloDekorator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDekorator;
+
+Console.WriteLine("Hello, World!");
+
+IComponent brot1 = new Käse( new Käse(new Salami(new Brot())));
+
+Console.WriteLine($"{brot1.Beschreibung} {brot1.Preis:c}");
+
+IComponent pizza = new Käse(new Salami(new Käse(new Pizza())));
+
+Console.WriteLine($"{pizza.Beschreibung} {pizza.Preis:c}");
+
