f3d4c6ddb06e9d7bce250cc1de81131ba18958b7
André <AndreR@ppedv.de>
2023-02-08 16:41:51 +0100

    Winforms Template Pattern

---
+++ WinFormsCutomControls/WinFormsCutomControls/MyButton.cs
@@ -0,0 +1,14 @@
+﻿namespace WinFormsCutomControls
+{
+    public class MyButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.FillRectangle(Brushes.Plum, pevent.ClipRectangle);
+            pevent.Graphics.FillEllipse(Brushes.SteelBlue, pevent.ClipRectangle);
+            
+
+        }
+    }
+
+}
