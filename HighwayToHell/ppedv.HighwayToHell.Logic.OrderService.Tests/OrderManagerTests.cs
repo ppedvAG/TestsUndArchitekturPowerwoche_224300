e8ace2c6432a6f7bb42ab6458ccbb423cc8c85a8
André <AndreR@ppedv.de>
2023-02-09 16:44:28 +0100

    OrderManager mock tests

--- HighwayToHell/ppedv.HighwayToHell.Logic.OrderService.Tests/OrderManagerTests.cs
+++ HighwayToHell/ppedv.HighwayToHell.Logic.OrderService.Tests/OrderManagerTests.cs
@@ -1,7 +1,33 @@
+using Moq;
+using ppedv.HighwayToHell.Model;
+using ppedv.HighwayToHell.Model.Contracts;
+
 namespace ppedv.HighwayToHell.Logic.OrderService.Tests
 {
     public class OrderManagerTests
     {
+
+        [Fact]
+        public void GetBestSellingMonth_December_wins()
+        {
+            var mock = new Mock<IRepository>();
+            mock.Setup(x => x.GetAll<Order>()).Returns(() =>
+            {
+                var o1 = new Order { OrderDate = new DateTime(2000, 4, 1) };
+                o1.Items.Add(new OrderItem { Amount = 4, Price = 12 });
+                var o2 = new Order { OrderDate = new DateTime(2000, 12, 1) };
+                o2.Items.Add(new OrderItem { Amount = 2, Price = 14 });
+                o2.Items.Add(new OrderItem { Amount = 2, Price = 12 });
+                return new[] {o1, o2 }; 
+            });
+
+            var om = new OrderManager(mock.Object);
+
+            var result = om.GetBestSellingMonth();
+
+            Assert.Equal(12, result);
+        }
+
         [Theory]
         [InlineData(0, 0, 0)]
         [InlineData(100, 0.2, 20)]
