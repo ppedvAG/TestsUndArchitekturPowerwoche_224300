27aab6d2ac0389c631ae3d42944e332707fd0ce3
André <AndreR@ppedv.de>
2023-02-09 16:34:54 +0100

    Dependency Injection mit AutoFac

--- HighwayToHell/ppedv.HighwayToHell.UI.DevConsole/Program.cs
+++ HighwayToHell/ppedv.HighwayToHell.UI.DevConsole/Program.cs
@@ -1,12 +1,31 @@
-﻿using ppedv.HighwayToHell.Logic.OrderService;
+﻿using Autofac;
+using ppedv.HighwayToHell.Data.EfCore;
+using ppedv.HighwayToHell.Logic.OrderService;
 using ppedv.HighwayToHell.Model;
 using ppedv.HighwayToHell.Model.Contracts;
+using System.Reflection;
 
 Console.WriteLine("*** HighwayToHell v0.1 ***");
 
 string conString = "Server=(localdb)\\mssqllocaldb;Database=HightwayToHell_dev;Trusted_Connection=true;";
-IRepository repo = new ppedv.HighwayToHell.Data.EfCore.EfRepository(conString);
-OrderManager om = new OrderManager(repo);
+
+//DI per Reflection
+//var path = @"C:\Users\Fred\source\repos\ppedvAG\TestsUndArchitekturPowerwoche_224300\HighwayToHell\ppedv.HighwayToHell.Data.EfCore\bin\Debug\net6.0\ppedv.HighwayToHell.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(path);
+//var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//var repo = Activator.CreateInstance(typeMitRepo, conString) as IRepository;
+
+//DI per AutoFac
+var builder = new ContainerBuilder();
+builder.RegisterType<EfRepository>().WithParameter("conString",conString).As<IRepository>();
+var container = builder.Build();    
+
+OrderManager om = new OrderManager(container.Resolve<IRepository>());
+IRepository repo = container.Resolve<IRepository>();    
+
+//DI Manuell 
+//IRepository repo = new ppedv.HighwayToHell.Data.EfCore.EfRepository(conString);
+//OrderManager om = new OrderManager(repo);
 
 int bestMonth = om.GetBestSellingMonth();
 Console.WriteLine($"Best Month: {new DateTime(1, bestMonth, 1):MMMM}");
@@ -16,6 +35,7 @@ foreach (var car in repo.GetAll<Car>())
 {
     Console.WriteLine($"{car.Manufacturer?.Name} {car.Model}");
 }
+
 Console.WriteLine("--- ORDERS ---");
 foreach (var order in repo.GetAll<Order>())
 {
